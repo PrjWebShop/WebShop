689e61e6d4741b8527b21ed982e230f3bd690caf
Alex <michauda16@gmail.com>
2022-02-25 13:20:57 -0500

    Added constants for Duplicate and Data too long sql errors

--- src/lib.php
+++ src/lib.php
@@ -1,4 +1,7 @@
 <?php
 
+define("SQL_ERROR_DUPLICATE", 1062);
+define("SQL_ERROR_DATA_TOO_LONG", 1406); // Data exceeds character limit
+
 require_once 'Account.cls.php';
 require_once 'Product.cls.php';
