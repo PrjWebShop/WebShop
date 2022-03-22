30932def6d40e134bdb5e5b83173e5467d10ec59
dokito11 <michauda16@gmail.com>
2022-03-22 18:05:56 -0400

    removed redundant clutter in product.php

--- product.php
+++ product.php
@@ -2,18 +2,5 @@
 
 require_once 'src/lib.php';
 
-// Checks if there is an id parameter and sends the user back to index if there isn't one
-if (!isset($_GET["id"]))
-{ header("Location: Index.php"); }
-
-// Checks if the user is logged in and retrieves account information if they are
-if (isset($_COOKIE["user"])) {
-    $user = Account::getAccountInfo($_COOKIE["user"]);
-    $accountLogged = true;
-}
-else {
-    $accountLogged = false;
-}
-
 $prod_id = $_GET["id"];
 $prod = Product::getProductByID($prod_id);
