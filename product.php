59cba3e2a027c01e17abf6fc694202c359b2f8e3
dokito11 <michauda16@gmail.com>
2022-04-06 21:45:41 -0400

    Added scripts into header and removed login.php

--- product.php
+++ product.php
@@ -11,15 +11,7 @@ if (isset($_GET["id"]))
 ?>
 <html>
 
-<head>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-    </style>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./Css/style.css">
-    <link rel="stylesheet" href="./Css/index.css">
-</head>
+<?php htmlHeader(__FILE__, $prod->getName()); ?>
 
 <body>
 
